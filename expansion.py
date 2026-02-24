beeb37c588e329b0b897d4e8fe58c9dc468a962f
YannSimon2 <yann.simon1805@gmail.com>
2026-02-24 22:44:56 +0100

    addings

--- expansion.py
+++ expansion.py
@@ -2,16 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.constants import c
 
-needle_size_px =
-u_needle_size_px=
+needle_size_px = 1200
+u_needle_size_px= 30
 needle_size_m = 1.20e-3
 u_needle_size_m = 0.02e-3
 
+conversion_factor = needle_size_m / needle_size_px
 
-plasma_size_px = np.array([])
+plasma_size_longi_px = np.array([])
+u_plasma_size_longi_px = 10
 
-plasma_size_m= np.array([])
+plasma_size_transv_px = np.array([])
+u_plasma_size_transv_px = 10
+
+
+plasma_size_longi_m= plasma_size_longi_px * conversion_factor
+u_plasma_size_longi_m = plasma_size_longi_m * np.sqrt((u_plasma_size_longi_px / plasma_size_longi_px) ** 2 + (u_needle_size_px / needle_size_px) ** 2 + (u_needle_size_m / needle_size_m) ** 2)
+
+plasma_size_transv_m= plasma_size_transv_px * conversion_factor
+u_plasma_size_transv_m = plasma_size_transv_m * np.sqrt((u_plasma_size_transv_px / plasma_size_transv_px) ** 2 + (u_needle_size_px / needle_size_px) ** 2 + (u_needle_size_m / needle_size_m) ** 2)
 
 delays_m = np.array([])
+u_delays_m = 0.01
 
 delays_s = np.array([])/c
