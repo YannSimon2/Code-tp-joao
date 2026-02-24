3f9b5d38bdd4e2e0a89818b34aa94bce78474417
YannSimon2 <yann.simon1805@gmail.com>
2026-02-24 20:54:12 +0100

    file creation

---
+++ expansion.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.constants import c
+
+needle_size_px =
+u_needle_size_px=
+needle_size_m = 1.20e-3
+u_needle_size_m = 0.02e-3
+
+
+plasma_size_px = np.array([])
+
+plasma_size_m= np.array([])
+
+delays_m = np.array([])
+
+delays_s = np.array([])/c
