9e8f1b52b7921f7fd78a9809d0f398ccffc52780
YannSimon2 <yann.simon1805@gmail.com>
2026-03-01 12:18:55 +0100

    energy dependance

---
+++ energy.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+energy = np.array([0.5,1,1.5,2,2.67,3.7,4.65,5.7,7.6,8.7,9.5,10.6])*1e-3
+u_energy = 0.05
+
+plasma_size_longi_px = np.array([125,167,166,188])
+u_plasma_size_longi_px = np.array([10])
+
+plasma_size_transv_px = np.array([217,252,287,303])
+u_plasma_size_transv_px = np.array([10])
+
